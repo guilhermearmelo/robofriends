146fff75f22b04e9cafb655e57f6aaa125a05591
Guilherme A. Melo <guilhermearmelo@gmail.com>
2019-02-25 14:29:51 -0300

    Updates

---
+++ precache-manifest.a7c03c83dfd34a4b6b26c5c32b63f2ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "a5f6c810f661933d672d",
+    "url": "/robofriends/static/js/main.a5f6c810.chunk.js"
+  },
+  {
+    "revision": "9193ce3e3f143ecfb787",
+    "url": "/robofriends/static/js/1.9193ce3e.chunk.js"
+  },
+  {
+    "revision": "a5f6c810f661933d672d",
+    "url": "/robofriends/static/css/main.50939e9a.chunk.css"
+  },
+  {
+    "revision": "9193ce3e3f143ecfb787",
+    "url": "/robofriends/static/css/1.ecd37d79.chunk.css"
+  },
+  {
+    "revision": "8bb5015ec7e76ae6e38953586dd00f6d",
+    "url": "/robofriends/index.html"
+  }
+];
